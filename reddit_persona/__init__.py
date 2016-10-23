a5a7f7ca11a769fce273e8014bc7c543d64031e6
Zachary A Estela <zestela@gmail.com>
2016-10-23 01:39:38 -0700

    docstring, version. bugfixes

--- reddit_persona/__init__.py
+++ reddit_persona/__init__.py
@@ -1,7 +1,11 @@
+""" This module uses APIs from Reddit and Indico.io to provide insights into a Redditor's personality attributes.
+Warning: This is just for fun, do not take results seriously. It is an experiment in combining social media data with machine learning APIs"""
+__version__="1.0.4"
 
 from reddit_persona import reddit_get,io_helper,insights, keycheck
 from reddit_persona.go import go
 from reddit_persona.keycheck import new_key, test_key, get_key
 __all__ = ['reddit_get','io_helper','insights','go','keycheck']
+
 
 
